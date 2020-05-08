2940dbc353138b942a03dfcc9724f68e3a5fd62d
Fabian David Schmidt <fabian.david.schmidt@hotmail.de>
2020-05-08 11:30:49 +0200

    Adding cuda setup

---
+++ setup-nvidia-cuda.sh
@@ -0,0 +1,18 @@
+# remove and update
+sudo rm /etc/apt/sources.list.d/cuda*
+sudo apt remove --autoremove nvidia-cuda-toolkit
+sudo apt remove --autoremove nvidia-*
+sudo apt update
+# add repo
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update
+# install driver
+sudo apt install nvidia-driver-440
+# fetch keys
+sudo apt-key adv --fetch-keys  http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
+sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda_learn.list'
+sudo apt update
+# install cuda and cudnn
+sudo apt install cuda-10-2
+sudo apt install libcudnn7
