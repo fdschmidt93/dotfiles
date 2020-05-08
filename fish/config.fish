a87b1094ac2e50ef7ae74459c22f186815b618d8
Fabian David Schmidt <fabian.david.schmidt@hotmail.de>
2020-05-08 11:59:03 +0200

    Adding fish

---
+++ fish/config.fish
@@ -0,0 +1,5 @@
+set -gx PATH /home/fdschmidt/miniconda3/bin ~/.fzf/bin/ /usr/local/cuda-10.2/bin/ $PATH
+set -x CPLUS_INCLUDE_PATH /usr/include/c++/7/ 
+set -x LD_LIBRARY_PATH /usr/local/lib/ /home/fdschmidt/intel/mkl/lib/intel64/ /usr/local/cuda-10.2/lib64/ $LD_LIBRARY_PATH
+set -gx FZF_DEFAULT_COMMAND  'rg --files --no-ignore-vcs --hidden'
+set -gx PKG_CONFIG_PATH /usr/local/lib/pkgconfig
--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -1,5 +1,6 @@
 echo "Linking files"
 ln -nsf $PWD/nvim $HOME/.config/nvim
 ln -nsf $PWD/alacritty $HOME/.config/alacritty
+ln -nsf $PWD/fish $HOME/.config/
 ln -nsf $PWD/tmux.conf $HOME/.tmux.conf
 echo "Success"
