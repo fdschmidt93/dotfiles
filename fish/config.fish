40c40a78e21d31ef049b4d0172a8894b46c5ac63
Fabian David Schmidt <fabian.david.schmidt@hotmail.de>
2020-05-09 13:24:59 +0200

    Updating settings

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-set -gx PATH /home/fdschmidt/miniconda3/bin ~/.fzf/bin/ /usr/local/cuda-10.2/bin/ $PATH
+set -gx PATH /home/fdschmidt/miniconda3/bin ~/.fzf/bin/ /usr/local/cuda-10.2/bin/ /home/fdschmidt/.dotnet/ $PATH
 set -x CPLUS_INCLUDE_PATH /usr/include/c++/7/ 
 set -x LD_LIBRARY_PATH /usr/local/lib/ /home/fdschmidt/intel/mkl/lib/intel64/ /usr/local/cuda-10.2/lib64/ $LD_LIBRARY_PATH
 set -gx FZF_DEFAULT_COMMAND  'rg --files --no-ignore-vcs --hidden'
