6572806b0432f8320038d94b50fdf773f6f5aa08
Fabian David Schmidt <fabian.david.schmidt@hotmail.com>
2020-06-17 14:51:22 +0200

    Update for Arch

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
-set -gx EDITOR /usr/local/bin/nvim
+set -gx EDITOR /usr/bin/nvim
 set -gx FZF_DEFAULT_COMMAND  'rg --files --no-ignore-vcs --hidden'
-set -gx PATH /home/fdschmidt/miniconda3/bin ~/.fzf/bin/ /usr/local/cuda-10.2/bin/ /home/fdschmidt/.dotnet/ $PATH
-set -gx PKG_CONFIG_PATH /usr/local/lib/pkgconfig
-set -x CPLUS_INCLUDE_PATH /usr/include/c++/7/ 
-set -x LD_LIBRARY_PATH /usr/local/lib/ /home/fdschmidt/intel/mkl/lib/intel64/ /usr/local/cuda-10.2/lib64/ $LD_LIBRARY_PATH
+set -gx PATH ~/.fzf/bin/ /opt/cuda/bin/ $PATH
+set -x LD_LIBRARY_PATH /usr/local/lib/ /home/fdschmidt/intel/mkl/lib/intel64/ /opt/cuda/lib64/ $LD_LIBRARY_PATH
 alias vi=nvim
+ source /home/fdschmidt/miniconda3/etc/fish/conf.d/conda.fish
+
