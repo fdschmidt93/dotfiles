dc3c01e8dc00080006d3d64c6242d3db9fd9e2c3
Fabian David Schmidt <fabian.david.schmidt@gmail.com>
2023-11-21 16:32:56 +0100

    chore(nvim): add flash

---
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {},
+  -- stylua: ignore
+  keys = {
+    -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    -- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "s", mode = { "n", }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+}
