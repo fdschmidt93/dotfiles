40c3c7f0080e32b15c966048804672df9e480780
Fabian David Schmidt <fabian.david.schmidt@gmail.com>
2025-08-19 15:40:56 +0200

    update fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,5 @@
-set -gx PATH ~/.fzf/bin/ /home/fdschmidt/.cargo/bin /home/fdschmidt/.local/bin/ $PATH
+set -gx PATH ~/.fzf/bin/ $HOME/.cargo/bin $HOME/.local/bin/ $PATH
 set -x LD_LIBRARY_PATH /usr/local/lib/ $LD_LIBRARY_PATH
-set -gx ALPHAVANTAGE_API_KEY "ZT0B722UX6NBPZME"
 set -gx EDITOR /usr/bin/nvim
 set -x INITIAL_QUERY ""
 set -x RG_PREFIX "rg --column --line-number --no-heading --color=always --smart-case "
@@ -13,25 +12,12 @@ set -gx FZF_ALT_C_COMMAND "fd --hidden -t d . $HOME"
 set -gx MANPAGER 'nvim +Man!'
 set -gx MANWIDTH 999
 alias vi=nvim
-alias vip="nvim '+PythonTerm'"
-eval (keychain --eval --agents ssh -Q --quiet id_rsa --nogui --noask)
 fish_vi_key_bindings
 set PYDEVD_IPYTHON_COMPATIBLE_DEBUGGING 1
 
 abbr ca conda activate
 abbr za zathura
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f $HOME/miniforge3/bin/conda
-    eval $HOME/miniforge3/bin/conda "shell.fish" "hook" $argv | source
-end
-
-if test -f "$HOME/miniforge3/etc/fish/conf.d/mamba.fish"
-    source "$HOME/miniforge3/etc/fish/conf.d/mamba.fish"
-end
-
-# <<< conda initialize <<<
+abbr winx xfreerdp3 /cert:tofu /sound /microphone +home-drive /sec:nla /d: /u:fdschmidt /p:RsvkMnnef29vogq /scale:100 +auto-reconnect /wm-class:"Windows" /v:127.0.0.1 /dynamic-resolution -grab-keyboard
 if status is-login
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
         exec startx -- -keeptty
